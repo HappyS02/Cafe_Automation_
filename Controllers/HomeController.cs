677e944a4b127e202ad6c4219accdffcb1633332
HappyS02 <ipkmtlu@gmail.com>
2025-12-10 14:17:24 +0300

    v.0.3
    menü & değerlendirme modeli oluşturdum

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,33 @@
-using System.Diagnostics;
+using CafeOtomasyon.Data;
 using CafeOtomasyon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Diagnostics;
 
 namespace CafeOtomasyon.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // Veritabaný baðlantýsý
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Menu()
+        {
+            // Sadece Aktif ürünleri getiriyoruz
+            var products = await _context.Products
+                .Include(p => p.Category) // Kategori bilgisini de al
+                .Where(p => p.IsActive)   // Sadece aktif olanlar
+                .OrderBy(p => p.Category.Name) // Kategoriye göre sýrala
+                .ToListAsync();
+
+            return View(products);
         }
 
         public IActionResult Index()
